bf32fa52e55091259ef96fcc14b0e12ea80a5a4a
Hien Le <quang-hien.le.hsj@ssss.gouv.qc.ca>
2023-10-23 16:24:53 -0400

    Change behaviour of archive_PRAGMatIQ.sh

--- pipeline_illumina/archive_PRAGMatIQ.sh
+++ pipeline_illumina/archive_PRAGMatIQ.sh
@@ -5,12 +5,35 @@
 # Archive PRAGMatIQ Runs from Emedgenes AWS S3 bucket to narval
 # ssh narval.calculquebec.ca
 #
-cd ${HOME}/projects/ctb-rallard/COMMUN/PRAGMatIQ-EMG
-count=1
+samples=$@
 total_samples=${#@}
-for sample in ${@}; do
+#samples=()
+#total_samples=${#samples[@]}
+
+cd ${HOME}/projects/ctb-rallard/COMMUN/PRAGMatIQ-EMG
+cp archive_PRAGMatIQ.log archive_PRAGMatIQ.log0
+
+count=1
+for sample in ${samples[@]}; do
     echo "Processing sample ${count}/${total_samples}, ${sample}"
     aws s3 --profile emedgene cp s3://cac1-prodca-emg-auto-results/CHU_Sainte_Justine/${sample}/ ./archives/${sample} --recursive
     echo ${sample} $( date +'%Y-%m-%d %T' ) >> archive_PRAGMatIQ.log
     ((count++))
 done
+
+samples=(GM231982
+GM232029
+GM232109
+GM231994
+GM232017
+GM232061
+GM232118
+GM232120
+GM232125
+GM232132
+GM232145
+GM232135
+GM232134
+GM232141
+GM232133
+)
