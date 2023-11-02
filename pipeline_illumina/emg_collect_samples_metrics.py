e5013ae4a4da4e2e5d7817d0ecb6ada8c1c73add
Hien Le <quang-hien.le.hsj@ssss.gouv.qc.ca>
2023-11-01 22:11:29 -0400

    Add TODO Nit to clobber log S3 files

--- pipeline_illumina/emg_collect_samples_metrics.py
+++ pipeline_illumina/emg_collect_samples_metrics.py
@@ -87,6 +87,9 @@ def download_emg_s3_logs(sample, profile='emedgene', logsdir='emg_logs'):
     - profile: AWS credentials, either `emedgene` or `emedgene-eval` [str]
     - returns: Downloaded files under folder `./emg_logs/` [list]
     """
+
+    # TODO: DO NOT CLOBBER EXISTING FILES, IN CASES WHERE MORE THAN ONE ANALYSIS EXISTS
+
     os.mkdir(logsdir) if not os.path.isdir(logsdir) else None
     site = 's3://cac1-prodca-emg-auto-results'
     domains = {'emedgene': 'CHU_Sainte_Justine', 'emedgene-eval': 'Ste_Justine_eval'}
