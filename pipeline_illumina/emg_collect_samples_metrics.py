ecb7c0b09020ec5337f948e4322651345f922e84
Hien Le <quang-hien.le.hsj@ssss.gouv.qc.ca>
2023-11-01 22:13:15 -0400

    Fix formatting

--- pipeline_illumina/emg_collect_samples_metrics.py
+++ pipeline_illumina/emg_collect_samples_metrics.py
@@ -340,7 +340,7 @@ def main(args):
         df_metrics    = pd.concat([df_metrics, get_metrics_from_log(sample, log_files=log_files)], ignore_index=True)
 
         coverage_files = glob_files(f"{args.dir}/{sample}.dragen.bed_coverage_metrics.csv")
-        df_coverages  = pd.concat([df_coverages, get_coverage_metrics(sample, coverage_files=coverage_files)], ignore_index=True)
+        df_coverages   = pd.concat([df_coverages, get_coverage_metrics(sample, coverage_files=coverage_files)], ignore_index=True)
 
         vcf_files     = glob_files(f"{args.dir}/{sample}.dragen.cnv.vcf.gz")
         df_cnvs       = pd.concat([df_cnvs, count_cnv(sample, vcf_files=vcf_files)], ignore_index=True)
