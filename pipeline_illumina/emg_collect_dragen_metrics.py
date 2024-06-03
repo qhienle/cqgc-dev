c5caca00df2d8080b65bc119b28deeb3a1af900d
Hien Le <quang-hien.le.hsj@ssss.gouv.qc.ca>
2024-06-03 09:15:28 -0400

    Work on upload

--- pipeline_illumina/emg_collect_dragen_metrics.py
+++ pipeline_illumina/emg_collect_dragen_metrics.py
@@ -229,7 +229,7 @@ def main(args):
     # some information collected here will be used for case creation later on
     # Emedgene.
     #
-    nq   = Nanuq()
+    nq = Nanuq()
 
     fc_parts = nq.parse_run_name(args.run)
     fc_date  = fc_parts[0]
@@ -283,6 +283,7 @@ def main(args):
     df_samples_families = df_samples_families.sort_values(by=['family_id', 'relation'], ascending=[True, False])
     df_samples_families['birthdate'] = pd.to_datetime(df_samples_families['birthdate'], format='%d/%m/%Y')
     df_samples_families['flowcell_date'] = pd.to_datetime(fc_date, format='%Y%m%d')
+    df_samples_families['flowcell'] = args.run
     df_samples_families.to_csv('samples_list.csv', index=None)
 
     df_samples_metrics  = pd.DataFrame.from_dict(samples_metrics, orient="index")
--- pipeline_illumina/emg_upload_fastqs.py
+++ pipeline_illumina/emg_upload_fastqs.py
@@ -2,7 +2,7 @@
 """
 Upload FASTQ files to BaseSpace.
 
-USAGE: emg_upload_fastqs.py <RUN>
+USAGE: emg_upload_fastqs.py
        emg_upload_fastqs.py 20240510_LH00336_0043_A22K5KMLT3
        emg_upload_fastqs.py --help
 
@@ -60,8 +60,7 @@ project_ids = {'CHUSJ': '3703702',
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Upload FASTQ files to BaseSpace. for a given Run.")
-    parser.add_argument('run', help="Run ID, ex: '20240510_LH00336_0043_A22K5KMLT3'")
-    parser.add_argument('--file', '-f', help="Get samples from `--file` instead of fetching <Run> from Nanuq.")
+    parser.add_argument('--file', '-f', default="samples_list.csv", help="Get samples from file. Default='samples_list.csv'.")
     parser.add_argument('--logging-level', '-l', dest='level', default='info',
                         help="Logging level (str), can be 'debug', 'info', 'warning'. Default='info'")
     return(parser.parse_args())
@@ -83,89 +82,9 @@ def configure_logging(level):
                         datefmt='%Y-%m-%d@%H:%M:%S')
 
 
-def nanuq_samples_to_df(run):
-    """
-    For a run in Nanuq, save samples infos to pd.DataFrame
-    """
-    samplenames = nq.list_samples(run)
-    cases = []
-    for cqgc_sample in samplenames:
-        cqgc, sample = cqgc_sample[0], cqgc_sample[1]
-
-        # Get information for sample from Nanuq for "cqgc" ID
-        #
-        try:
-            data = json.loads(nq.get_sample(cqgc))
-        except Exception as e:
-            logging.warning(f"JSONDecodeError {e} could not decode sample {cqgc} ({sample})")
-            continue
-
-        logging.info(f"Got information for biosample {cqgc} a.k.a. {sample}")
-        if len(data) != 1:
-            logging.debug(f"Number of samples retrieved from Nanuq is not 1.\n{data}")
-        
-        # Verify that Nanuq info is the same as our cqgc and sample IDs
-        #
-        if cqgc != data[0]["labAliquotId"]:
-            logging.error(f"CQGC ID {cqgc} does not match identifier from Nanuq {data[0]['labAliquotId']}")
-        elif sample != '':
-            if sample != data[0]["ldmSampleId"]:
-                logging.error(f"Lab sample {sample} does not match identifier from Nanuq {data[0]['ldmSampleId']}")
-        else:
-            pass
-
-        try:
-            data[0]["patient"]["mrn"]
-        except Exception as err:
-            logging.warning(f"Could not find MRN for patient {cqgc} ({sample}: {err})")
-            data[0]["patient"]["mrn"] = '0000000'
-        else:
-            pass
-        
-        # try:
-        #     birthDate = datetime.datetime.strptime(data[0]["patient"]["birthDate"], '%d/%m/%Y').strftime('%Y-%m-%d')
-        # except:
-        #     logging.warning(f"Could not convert birthDate {data[0]['patient']['birthDate']}")
-        #     birthDate = data[0]["patient"]["birthDate"]
-
-        finally:
-            sample_infos = [
-                data[0]["ldmSampleId"],
-                data[0]["labAliquotId"],
-                data[0]["patient"]["familyMember"],
-                data[0]["patient"]["sex"],
-                data[0]["patient"]["ep"],
-                data[0]["patient"]["mrn"],
-                data[0]["patient"]["designFamily"],
-                data[0]["patient"]["birthDate"],
-                data[0]["patient"]["status"],
-                data[0]["patient"].get("familyId", "-")
-            ]
-        cases.append(sample_infos)
-
-    # Load cases (list of list) in a DataFrame, sort and group members
-    # Translate column names to match EMG's manifest specifications.
-    # pid => Family Id, hpo_labels => phenotypes, hpo_ids => hpos
-    # Group by family and sort by relation.
-    # Add Family Id (PID) to all family members based on familyID.
-    #
-    df = pd.DataFrame(cases)
-    df.columns = ['sample_name', 'biosample', 'relation', 'gender', 'label', 
-                  'mrn', 'cohort_type', 'date_of_birth(YYYY-MM-DD)', 'status',
-                  'Family Id']
-    df['date_of_birth(YYYY-MM-DD)'] =  pd.to_datetime(df['date_of_birth(YYYY-MM-DD)'], format='%d/%m/%Y')
-    return df
-
-
 def main(args):
     """
     """
-    fc_parts = nq.parse_run_name(args.run)
-    fc_date  = fc_parts[0]
-    fc_short = fc_parts[4]
-    print(f"# Logging run {fc_parts}")
-    
-    workdir  = f"{os.getcwd()}{os.sep}{fc_short}"
     fastqdir = f"/staging/hiseq_raw/{fc_short}/{args.run}/Analysis/1"
     try:
         os.mkdir(workdir)
