1a4692d5105706e88202cad1ecd8026a5f888646
Hien Le <quang-hien.le.hsj@ssss.gouv.qc.ca>
2024-12-22 08:24:22 -0500

    Change redirectong output for emg batch log

--- pipeline_illumina/run_pipeline_prag.sh
+++ pipeline_illumina/run_pipeline_prag.sh
@@ -47,7 +47,7 @@ touch ${WORKDIR}/${FC}/UploadBsComplete.txt
 ## 3. Créer les cas sur Emedgene 
 ###  3.1. Générer le fichier "emg_batch_manifest.csv" `emg_make_batch_from_nanuq.py ${FC}`
 ###  3.2. Glisser-déposer dans Emedgene le fichier "emg_batch_manifest.csv"
-# python ${SOFTDIR}/emg_make_batch_from_nanuq.py ${FC_SHORT} 2>&1 | tee ${FC_SHORT}/emg_make_batch.log
+# python ${SOFTDIR}/emg_make_batch_from_nanuq.py ${FC_SHORT} >> ${WORKDIR}/${FC}/emg_make_batch.log  2>&1
 
 
 ## 4. Collecter les metriques
