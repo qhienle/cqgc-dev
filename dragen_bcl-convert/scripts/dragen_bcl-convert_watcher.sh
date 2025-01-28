451b3b3c9440afc436d137c4d09fa82d1c1c7a88
Hien Le <quang-hien.le.hsj@ssss.gouv.qc.ca>
2025-01-28 15:32:08 -0500

    Improve logging, add prefix

--- dragen_bcl-convert/scripts/dragen_bcl-convert_watcher.sh
+++ dragen_bcl-convert/scripts/dragen_bcl-convert_watcher.sh
@@ -14,7 +14,11 @@
 
 BASEDIR='/mnt/spxp-app02/staging/hiseq_raw'
 WORKDIR='/mnt/spxp-app02/staging2/dragen'
+LOGFILE="${WORKDIR}/dragen_bcl-convert_watcher.log"
+LOGPREFIX="[bcl-watcher]"
 WATCHDIRS=("${BASEDIR}/A00516" "${BASEDIR}/LH00336" "${BASEDIR}/A00977" "${BASEDIR}/LH00207R" "/mnt/vs_nas_chusj/SPXP_APP02_NFS/LH00336")
+
+printf "\n\n######\n%s %s %s\n######\n\n" $0 ${LOGPREFIX} $( date "+%F[%T]" ) | tee ${LOGFILE}
 
 launch_run() {
     # Check if sequencing is finished (CopyComplete.txt) and that run
@@ -25,27 +29,27 @@ launch_run() {
     parts=($(echo ${fc} | tr '_' '\n'))
     fc_short="${parts[1]}_${parts[2]}"
     if [[ -f "${dir}/${fc}/CopyComplete.txt" ]]; then
-        echo "${dir}/${fc}/CopyComplete.txt file indicates that sequencing has finished"
+        echo "${LOGPREFIX} ${dir}/${fc}/CopyComplete.txt file indicates that sequencing has finished"
         if [[ -d ${WORKDIR}/${fc} ]]; then
-            echo "PASS: Run already processed in ${WORKDIR}/${fc}"
+            echo "${LOGPREFIX} PASS: Run already processed in ${WORKDIR}/${fc}"
         else
             mkdir ${WORKDIR}/${fc}
             cd ${WORKDIR}/${fc}
-            echo "Getting SampleSheet and other files from Nanuq..."
+            echo "${LOGPREFIX} Getting SampleSheet and other files from Nanuq..."
             # if [[ ! -f ${WORKDIR}/${FC} ]]; then
             # TODO: Skip get_nanuq_files if SampleSheet already present?
             # fi
             python /staging2/soft/CQGC-utils/Helpers/get_nanuq_files.py --run ${fc_short}
             if [[ -f "${WORKDIR}/${fc}/SampleSheet.csv" ]]; then
-                echo "${dir}/${fc}/: Launching BCL-convert with qsub..."
+                echo "${LOGPREFIX} ${dir}/${fc}/: Launching BCL-convert with qsub..."
                 qsub /staging2/soft/CQGC-utils/Analysis.dragen_bcl-convert/scripts/dragen_bcl-convert_launcher.sh ${fc}
             else
-                echo "ERROR: SampleSheet.csv not found in ${WORKDIR}/${fc}" >&2
+                echo "${LOGPREFIX} ERROR: SampleSheet.csv not found in ${WORKDIR}/${fc}" >&2
                 exit 1
             fi
         fi
     else
-        echo "${dir}/${fc}: Sequencing not finished. Waiting for CopyComplete.txt"
+        echo "${LOGPREFIX} ${dir}/${fc}: Sequencing not finished. Waiting for CopyComplete.txt"
     fi
 }
 
@@ -57,22 +61,22 @@ for dir in ${WATCHDIRS[@]}; do
             # Check SampleSheet if run is LowPass.
             # If no SampleSheet is found, then run is not LowPass
             echo "---------------------------------"
-            echo "${FC}"
+            echo "${LOGPREFIX} ${FC}"
             if [[ -f "${dir}/${FC}/SampleSheet.csv" ]]; then
                 if grep -q "LowPass" "${dir}/${FC}/SampleSheet.csv"; then
-                    echo "PASS: ${dir}/${FC}: Found the word LowPass in SampleSheet"
+                    echo "${LOGPREFIX} PASS: ${dir}/${FC}: Found the word LowPass in SampleSheet"
                 else
-                    echo "${dir}/${FC}: SampleSheet exists and not for LowPass."
+                    echo "${LOGPREFIX} ${dir}/${FC}: SampleSheet exists and not for LowPass."
                     launch_run ${dir} ${FC}
                 fi
             elif [[ -f "${dir}/${FC}/LowPass*.csv" ]]; then
-                echo "PASS: ${dir}/${FC} Found what looks like a LowPass SampleSheet."
+                echo "${LOGPREFIX} PASS: ${dir}/${FC} Found what looks like a LowPass SampleSheet."
             elif [[ -f "${dir}/${FC}/Failed.txt" ]] ||  [[ -f "${dir}/${FC}/failed.txt" ]]; then
-                echo "PASS: ${dir}/${FC}/Failed.txt marks a failed Run."
+                echo "${LOGPREFIX} PASS: ${dir}/${FC}/Failed.txt marks a failed Run."
             elif [[ -f "${dir}/${FC}/FastqComplete.txt" ]]; then
-                echo "PASS: ${dir}/${FC}/FastqComplete.txt indicates that run has already been processed."
+                echo "${LOGPREFIX} PASS: ${dir}/${FC}/FastqComplete.txt indicates that run has already been processed."
             else
-                echo "${dir}/${FC}: Could not find ${dir}/${FC}/SampleSheet.csv."
+                echo "${LOGPREFIX} ${dir}/${FC}: Could not find ${dir}/${FC}/SampleSheet.csv."
                 launch_run ${dir} ${FC}
             fi
         fi
@@ -80,4 +84,4 @@ for dir in ${WATCHDIRS[@]}; do
     done
     echo
 done
-exit
+exit 0
