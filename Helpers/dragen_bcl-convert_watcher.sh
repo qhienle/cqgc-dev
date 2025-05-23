5219dae5210e45e4453374006e4eeca5fe790f74
Hien Le <quang-hien.le.hsj@ssss.gouv.qc.ca>
2025-05-23 16:13:40 -0400

    Comment-out bs command

--- Helpers/dragen_bcl-convert_watcher.sh
+++ Helpers/dragen_bcl-convert_watcher.sh
@@ -62,8 +62,8 @@ for dir in ${WATCHDIRS[@]}; do
         parts=($(echo ${fc} | tr '_' '\n'))
         if [[ ${#parts[@]} -eq 4 ]]; then
             # `bs` for debug. Probably doesn't work in crontab
-            echo "${LOGPREFIX} $( bs -c cac1 list runs --format csv | grep ${fc} | sed -e 's/,/ \| /g')" 
-            # echo "${LOGPREFIX} ${fc}"
+            # echo "${LOGPREFIX} $( bs -c cac1 list runs --format csv | grep ${fc} | sed -e 's/,/ \| /g')" 
+            echo "${LOGPREFIX} ${fc}"
             if [[ -f "${dir}/${fc}/CopyComplete.txt" ]]; then
                 # echo "${LOGPREFIX} CopyComplete.txt indicates that sequencing has finished"
                 # Check if bcl-convert needed (not previously demuxed, not failed, not LowPass)
