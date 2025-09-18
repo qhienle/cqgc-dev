77b9e4e94048aee8004ed3b639eab155ff0dd568
Hien Le <quang-hien.le.hsj@ssss.gouv.qc.ca>
2025-09-18 15:23:16 -0400

    Work on emg_pipeline_watcher.sh

--- pipeline_illumina/emg_pipeline_watcher.sh
+++ pipeline_illumina/emg_pipeline_watcher.sh
@@ -3,7 +3,7 @@
 # Watcher to detect and run pipeline for submitting cases to Emedgene.
 # USAGE: bash run_pipeline_emg.sh
 #        bash run_pipeline_emg.sh | tee -a /mnt/vs_nas_chusj/CQGC_PROD/fastqs/emg_watcher.log
-
+#
 
 HISEQ_R='/mnt/spxp-app02/staging/hiseq_raw'
 BASEDIR='/mnt/vs_nas_chusj/CQGC_PROD/sequenceurs'
@@ -14,24 +14,37 @@ WATCHDIRS=("${BASEDIR}/A00516" "${BASEDIR}/LH00336" "${BASEDIR}/LH00207R" "${HIS
 LOGPREFIX="[emg-watcher]"
 LOGFILE="${WORKDIR}/emg_watcher.log"
 
+# Run the pipeline
+#
+run_pipeline_emg() {
+    local fc=$1
+    local project=$2
+    echo "${LOGPREFIX} ${project} bash ${SOFTDIR}/Analysis.pipeline_illumina/run_pipeline_prag.sh ${fc} 2>&1 | tee ${WORKDIR}/${fc}/run_pipeline_prag.log"
+}
+
 for dir in ${WATCHDIRS[@]}; do
     for fc in $( ls ${dir} ); do
+        cd ${WORKDIR}/${fc}
+        #
         # "RunName" in NovaSeq600's SampleSheet don't contain experiment name
         # (PRAG, Q1K,...) so better check Project Name in BaseSpace (with `bs`)
+        #
         xp=$( bs -c cac1 list runs --format csv | grep ${fc} | cut -d, -f3)
         if [[ ! -z ${xp} ]]; then
             if [[ -f "${WORKDIR}/${fc}/run_pipeline_emg.log" ]]; then
                 echo "${LOGPREFIX} PASS: Pipeline already processed or is running"
             elif [[ ! -z $( echo ${xp} | grep 'PRAG' ) ]]; then
-                echo "${LOGPREFIX} ${fc} is a run for PRAG"
-                echo "${LOGPREFIX} Launching pipeline for ${fc} (${xp})"
-                cd ${WORKDIR}/${fc}
-                echo 'bash ${SOFTDIR}/Analysis.pipeline_illumina/run_pipeline_prag.sh ${fc} 2>&1 | tee ${WORKDIR}/${fc}/run_pipeline_prag.log'
-                fi
+                echo "${LOGPREFIX} Launching pipeline for PRAG ${fc} | (${xp})"
+                run_pipeline_emg ${fc} 'prag'
             elif [[ ! -z $( echo ${xp} | grep 'Q1K' ) ]]; then
-                echo "${LOGPREFIX} ${fc} is a run fo Q1K"
+                echo "${LOGPREFIX} Launching pipeline for Q1K ${fc} | (${xp})"
+                run_pipeline_emg ${fc} 'q1k'
             elif [[ ! -z $( echo ${xp} | grep 'AOH' ) ]]; then
-                echo "${LOGPREFIX} ${fc} is a run fo AOH"
+                echo "${LOGPREFIX} Launching pipeline for AOH ${fc} | (${xp})"
+                run_pipeline_emg ${fc} 'aoh'
+            elif [[ ! -z $( echo ${xp} | grep 'C4R' ) ]]; then
+                echo "${LOGPREFIX} Launching pipeline for C4R ${fc} | (${xp})"
+                run_pipeline_emg ${fc} 'c4r'
             else
                 echo "${LOGPREFIX} Nothing to do for ${fc} (${xp})"
             fi
