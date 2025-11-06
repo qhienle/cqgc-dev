f222a9b1bd5df4aa914a89f6c545481d6fb8dd5a
Hien Le <quang-hien.le.hsj@ssss.gouv.qc.ca>
2025-11-06 15:13:51 -0500

    Update doc

--- pipeline_illumina/emg_pipeline_watcher.sh
+++ pipeline_illumina/emg_pipeline_watcher.sh
@@ -5,10 +5,10 @@
 #        bash run_pipeline_emg.sh | tee -a /mnt/vs_nas_chusj/CQGC_PROD/fastqs/emg_watcher.log
 #
 # Logs for the process are written to these files:
-#   - ${WORKDIR}/${fc}/run_pipeline_emg.log : logs for the pipeline
 #   - ${WORKDIR}/emg_watcher.log            : logs for this watcher
+#   - ${WORKDIR}/${fc}/run_pipeline_emg.log : logs for each pipeline
 #
-# The following files mark the different steps of the pipeline:
+# In addition, the following files mark the different steps of the pipeline:
 #   - ${BASEDIR}/${FC}/CopyComplete.txt  : end of sequencing run
 #   - ${BASEDIR}/${FC}/DemuxFailed.txt or "failed.txt" : sequencing has failed
 #   - ${BASEDIR}/${FC}/DemuxStarted.txt  : bcl-convert is in progress
@@ -25,7 +25,7 @@ LOGFILE="${WORKDIR}/emg_watcher.log"
 NAPTIME=900
 
 umask 002
-printf "\n\n######\n%s %s %s\n######\n\n" ${LOGPREFIX} $( date "+%F@%T" ) $0 #| tee -a ${LOGFILE}/
+printf "\n\n######\n%s %s %s\n######\n\n" ${LOGPREFIX} $( date "+%F@%T" ) $0 | tee -a ${LOGFILE}/
 
 # Run the pipeline
 #
@@ -52,11 +52,10 @@ run_pipeline_emg() {
 
 for dir in ${WATCHDIRS[@]}; do
     for fc in $( ls ${dir} ); do
-        #
-        # "RunName" in NovaSeq600's SampleSheet don't contain experiment name
+        # "RunName" in NovaSeq600's SampleSheet doesn't contain experiment name
         # (PRAG, Q1K,...) so better check Project Name in BaseSpace (with `bs`)
         #
-        xp=$( bs -c cac1 list runs --format csv | grep ${fc} | cut -d, -f3)
+        xp=$( bs -c cac1 list runs --format csv | grep ${fc} | cut -d, -f3 )
         if [[ ! -z ${xp} ]]; then
             if [[ -f "${WORKDIR}/${fc}/run_pipeline_emg.log" ]]; then
                 echo "${LOGPREFIX} PASS: Pipeline already processed or is running for ${fc} | ${xp}"
@@ -73,10 +72,10 @@ for dir in ${WATCHDIRS[@]}; do
                 echo "${LOGPREFIX} RUN: pipeline for C4R ${fc} | ${xp}"
                 run_pipeline_emg ${fc} 'c4r'
             else
-                true # echo "${LOGPREFIX} Nothing to do for ${fc} (${xp})"
+                echo "${LOGPREFIX} Nothing to do for ${fc} (${xp})"
             fi
         else
-            true #echo "${LOGPREFIX} Run ${fc} not found on BaseSpace."
+            echo "${LOGPREFIX} Run ${fc} not found on BaseSpace."
         fi
     done
 done
