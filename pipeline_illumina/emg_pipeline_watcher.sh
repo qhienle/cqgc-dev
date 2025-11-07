0cd258b85f171c3c38083958694f27b154af2eae
Hien Le <quang-hien.le.hsj@ssss.gouv.qc.ca>
2025-11-07 14:05:06 -0500

    verify if folder is a valid run

--- pipeline_illumina/emg_pipeline_watcher.sh
+++ pipeline_illumina/emg_pipeline_watcher.sh
@@ -52,31 +52,36 @@ run_pipeline_emg() {
 for dir in ${WATCHDIRS[@]}; do
     for fc in $( ls ${dir} ); do
         log="${WORKDIR}/${fc}/emg_pipeline.log"
-        # "RunName" in NovaSeq600's SampleSheet doesn't contain experiment name
-        # (PRAG, Q1K,...) so better check Project Name in BaseSpace (with `bs`)
-        #
-        xp=$( bs -c cac1 list runs --format csv | grep ${fc} | cut -d, -f3 )
-        if [[ ! -z ${xp} ]]; then
-            if [[ -f "${WORKDIR}/${fc}/run_pipeline_emg.log" ]]; then
-                echo "${LOGPREFIX} PASS: Pipeline already processed or is running for ${fc} | ${xp}" | tee -a ${LOGFILE}
-            elif [[ ! -z $( echo ${xp} | grep 'PRAG' ) ]]; then
-                echo "${LOGPREFIX} RUN: pipeline for PRAG ${fc} | ${xp}" | tee -a ${LOGFILE}
-                (run_pipeline_emg ${fc} 'prag' > ${log} 2>&1) &
-            elif [[ ! -z $( echo ${xp} | grep 'Q1K' ) ]]; then
-                echo "${LOGPREFIX} RUN: pipeline for Q1K ${fc} | ${xp}" | tee -a ${LOGFILE}
-                (run_pipeline_emg ${fc} 'q1k' > ${log} 2>&1) &
-            elif [[ ! -z $( echo ${xp} | grep 'AOH' ) ]]; then
-                echo "${LOGPREFIX} RUN: pipeline for AOH ${fc} | ${xp}" | tee -a ${LOGFILE}
-                (run_pipeline_emg ${fc} 'aoh' > ${log} 2>&1) &
-            elif [[ ! -z $( echo ${xp} | grep 'C4R' ) ]]; then
-                echo "${LOGPREFIX} RUN: pipeline for C4R ${fc} | ${xp}" | tee -a ${LOGFILE}
-                (run_pipeline_emg ${fc} 'c4r' > ${log} 2>&1) &
+        parts=($(echo ${fc} | tr '_' '\n'))
+        if [[ ${#parts[@]} -eq 4 ]]; then
+            # ${fc} looks valid. Check if it's an experiment for Emedgene.
+            # "RunName" in NovaSeq600's SampleSheet doesn't contain experiment name
+            # (PRAG, Q1K,...) so better check Project Name in BaseSpace (with `bs`)
+            #
+            xp=$( bs -c cac1 list runs --format csv | grep ${fc} | cut -d, -f3 )
+            if [[ ! -z ${xp} ]]; then
+                if [[ -f "${WORKDIR}/${fc}/run_pipeline_emg.log" ]]; then
+                    echo "${LOGPREFIX} PASS: Pipeline already processed or is running for ${fc} | ${xp}" | tee -a ${LOGFILE}
+                elif [[ ! -z $( echo ${xp} | grep 'PRAG' ) ]]; then
+                    echo "${LOGPREFIX} RUN: pipeline for PRAG ${fc} | ${xp}" | tee -a ${LOGFILE}
+                    (run_pipeline_emg ${fc} 'prag' > ${log} 2>&1) &
+                elif [[ ! -z $( echo ${xp} | grep 'Q1K' ) ]]; then
+                    echo "${LOGPREFIX} RUN: pipeline for Q1K ${fc} | ${xp}" | tee -a ${LOGFILE}
+                    (run_pipeline_emg ${fc} 'q1k' > ${log} 2>&1) &
+                elif [[ ! -z $( echo ${xp} | grep 'AOH' ) ]]; then
+                    echo "${LOGPREFIX} RUN: pipeline for AOH ${fc} | ${xp}" | tee -a ${LOGFILE}
+                    (run_pipeline_emg ${fc} 'aoh' > ${log} 2>&1) &
+                elif [[ ! -z $( echo ${xp} | grep 'C4R' ) ]]; then
+                    echo "${LOGPREFIX} RUN: pipeline for C4R ${fc} | ${xp}" | tee -a ${LOGFILE}
+                    (run_pipeline_emg ${fc} 'c4r' > ${log} 2>&1) &
+                else
+                    echo "${LOGPREFIX} Nothing to do for ${fc} (${xp})" | tee -a ${LOGFILE}
+                fi
             else
-                echo "${LOGPREFIX} Nothing to do for ${fc} (${xp})" | tee -a ${LOGFILE}
+                echo "${LOGPREFIX} Run ${fc} not found on BaseSpace." | tee -a ${LOGFILE}
             fi
-        else
-            echo "${LOGPREFIX} Run ${fc} not found on BaseSpace." | tee -a ${LOGFILE}
         fi
+        # else: ignore because format of folder name doesn't look like a run
     done
 done
 exit 0
