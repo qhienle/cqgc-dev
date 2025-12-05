7e380d2d6c96d59dc1a05f4d8fc52649a0b2083b
Hien LE <quang-hien.le.hsj@ssss.gouv.qc.ca>
2025-12-05 16:00:17 -0500

    Add test authenticatedHeaders

--- lib/tests/test_qlin.py
+++ lib/tests/test_qlin.py
@@ -11,12 +11,17 @@ from qlin import qlin
 
 class TestQlin(unittest.TestCase):
     def setUp(self):
-        pass
+        self.q = qlin('https://qlin-me-hybrid.cqgc.hsj.rtss.qc.ca')
     
     def test_load_url(self):
-        q = qlin('https://qlin-me-hybrid.cqgc.hsj.rtss.qc.ca')
-        self.assertIsNotNone(q.url, 'Server URL should not be NoneType')
-        self.assertIsInstance(q.url, str, 'qlin.url should be of type str')
+        # q = qlin('https://qlin-me-hybrid.cqgc.hsj.rtss.qc.ca')
+        self.assertIsNotNone(self.q.url, 'Server URL should not be NoneType')
+        self.assertIsInstance(self.q.url, str, 'qlin.url should be of type str')
+        
+    def test_attr_authenticatedHeaders(self):
+        self.assertIsNotNone(self.q.authenticatedHeaders, 'Server URL should not be NoneType')
+        self.assertIsInstance(self.q.authenticatedHeaders, dict, 'qlin.authenticatedHeaders should be of type dict')
+        self.assertIsInstance(self.q.authenticatedHeaders['Authorization'], str, 'qlin.authenticatedHeaders["Authorization"] should be of type str')
         
     def tearDown(self):
         pass
